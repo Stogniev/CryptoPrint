ec7f61543417fe428eec31f9627b91f7d1e56042
Eli Sklar <eli.s@coindash.io>
2017-11-21 21:45:54 +0200

    prv test

--- src/modules/cryptoprint/sections/print-view/index.js
+++ src/modules/cryptoprint/sections/print-view/index.js
@@ -5,7 +5,7 @@ import raws from './raws'
 export class PrintViewSection extends Component {
   render () {
     return <div>
-      <h2>Welcome to PrintView</h2>
+      <h2>Welcome to PrintView PRV</h2>
     </div>
   }
 }
