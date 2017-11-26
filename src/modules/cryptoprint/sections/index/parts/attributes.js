01ec56723c311c9ef5df7aeb70d7fb73b47c8605
Eli Sklar <eli.s@coindash.io>
2017-11-26 12:57:39 +0200

    orgz

--- src/modules/cryptoprint/sections/index/parts/attributes.js
+++ src/modules/cryptoprint/sections/index/parts/attributes.js
@@ -1,45 +1,45 @@
 import React, { Component } from 'react'
 
+const properties = [
+  {
+    title: 'Why paper wallets?',
+    text: 'When you keep your cryptocurrency in an online exchange or wallet, your private key remains vulnerable to theft. Introducing paper wallets, the safest way to store cryptocurrency. With a paper wallet, your private key exists only on paper and has never been online. No one can hack it, steal or do anything else with it without your explicit cooperation.',
+    image: '/images/Back.png'
+  },
+  {
+    title: 'Why Cryptoprint wallets?',
+    text: `Our wallets are affordable.
+      Designed to look like familiar fiat bills, our wallets are easy to use and understand for anyone, bringing cryptocurrencies to the masses.
+      Perfect for cold-storage: Ever wanted to stash your cryptocurrency somewhere safe? Now you can.`,
+    image: '/images/drmakete-lab-57353.png'
+  },
+  {
+    title: 'Not Just Bitcoin',
+    text: 'We offer wallets for each of the top 10 most popular cryptocurrencies today and will add support for more blockchains in the future',
+    image: '/images/Front.png'
+  },
+  {
+    title: 'Standard as a Standard',
+    text: 'By using standardized formatting (QA code, base58…) for the keys, our wallet is universally compatible with any wallets or hardware from day one.',
+    image: '/images/cristina-gottardi-177261 (1).png'
+  }
+
+]
+
 export class AttributesSection extends Component {
   render () {
     return <section className='attributes' id='attributes'>
       <h2>The World's Safest Paper Wallet</h2>
       <ul>
-        <li>
-          <img src='/images/Back.png' alt='tmp-img' />
-          <div>
-            <h3>Revolutionary Paper wallet.</h3>
-            <p>Talk about the product.</p>
-          </div>
-        </li>
-        <li>
-          <img src='/images/cristina-gottardi-177261 (1).png' alt='tmp-img' />
-          <div>
-            <h3>Visual cryptography</h3>
-            <p>Talk about printed cryptography, alingment... Why it's safe.</p>
-          </div>
-        </li>
-        <li>
-          <img src='/images/drmakete-lab-57353.png' alt='tmp-img' />
-          <div>
-            <h3>Fold to See</h3>
-            <p>How money is sent/received. Fold to reveal... etc...</p>
-          </div>
-        </li>
-        <li>
-          <img src='/images/Front.png' alt='tmp-img' />
-          <div>
-            <h3>Security</h3>
-            <p>Secure printing. 24 hour camera stream... Trustbuilding...</p>
-          </div>
-        </li>
-        <li>
-          <img src='/images/cristina-gottardi-177261 (1).png' alt='tmp-img' />
-          <div>
-            <h3>A Variety of Currencies</h3>
-            <p>Not just Bitcoin, but any of the top 20 cryptocurrencies today!</p>
-          </div>
-        </li>
+        {properties.map(({title, text, image}) => (
+          <li>
+            <img src={image} alt={title} />
+            <div>
+              <h3>{title}</h3>
+              {text.split('\n').map(e => <p>{e}</p>)}
+            </div>
+          </li>
+        ))}
       </ul>
     </section>
   }
