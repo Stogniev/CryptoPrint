60574420bcc7a64608e9d82a587ae57d3ff55f0b
Ideasoft <ideasoft@iMac-Ideasoft.local>
2018-01-03 17:53:32 +0200

    add responsive

--- src/modules/cryptoprint/sections/index/parts/footer.js
+++ src/modules/cryptoprint/sections/index/parts/footer.js
@@ -10,40 +10,44 @@ export class FooterSection extends Component {
   render () {
     return <footer className='footer'>
       <div className='footer-top'>
-        <div className='flex-container'>
-          <div>
-            <LogoOnepager />
-          </div>
-          <nav>
-            <h4>Sitemap</h4>
-            <ul>
-              <li><a href='/'></a></li>
-              <li><a href='/'></a></li>
-              <li><a href='/'></a></li>
-              <li><a href='/'></a></li>
-              <li><a href='/'></a></li>
-            </ul>
-          </nav>
-          <div className='resources'>
-            <h4>Resources</h4>
-            <button className='circle-btn'>White Paper</button>
-          </div>
-          <div className='contact'>
-            <h4>Contact us</h4>
-            <a href='email:contact@cryptoprint.io'>contact@cryptoprint.io</a>
-            <ul className='social-links'>
-              <li><a href='https://www.facebook.com/cryptoprint'><FacebookMonoIcon/></a></li>
-              {/*<li><a href='https://twitter.com/'><TwitterIcon/></a></li>*/}
-              <li><a href='https://t.me/cryptoprint'><TelegramMonoIcon/></a></li>
-              {/*<li><a href='www.slack.com/'><SlackIcon/></a></li>*/}
-            </ul>
+        <div className='container'>
+          <div className='flex-container'>
+            <div>
+              <LogoOnepager />
+            </div>
+            <nav>
+              <h4>Sitemap</h4>
+              <ul>
+                <li><a href='/'></a></li>
+                <li><a href='/'></a></li>
+                <li><a href='/'></a></li>
+                <li><a href='/'></a></li>
+                <li><a href='/'></a></li>
+              </ul>
+            </nav>
+            <div className='resources'>
+              <h4>Resources</h4>
+              <button className='circle-btn'>White Paper</button>
+            </div>
+            <div className='contact'>
+              <h4>Contact us</h4>
+              <a href='email:contact@cryptoprint.io'>contact@cryptoprint.io</a>
+              <ul className='social-links'>
+                <li><a href='https://www.facebook.com/cryptoprint'><FacebookMonoIcon/></a></li>
+                {/*<li><a href='https://twitter.com/'><TwitterIcon/></a></li>*/}
+                <li><a href='https://t.me/cryptoprint'><TelegramMonoIcon/></a></li>
+                {/*<li><a href='www.slack.com/'><SlackIcon/></a></li>*/}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
       <div className='footer-bottom'>
-        <div className='flex-container'>
-          <span className='copyright'>Copyright 2017 - Cryptoprint.io</span>
-          <span className='designed'>Designed by Porat Shalev &amp; Co.</span>
+        <div className='container'>
+          <div className='flex-container'>
+            <span className='copyright'>Copyright 2017 - Cryptoprint.io</span>
+            <span className='designed'>Designed by Porat Shalev &amp; Co.</span>
+          </div>
         </div>
       </div>
     </footer>
