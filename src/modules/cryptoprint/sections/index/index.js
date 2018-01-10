cd88be03529f4941dea5d31567a6aef0a06a7ad8
Ideasoft <ideasoft@iMac-Ideasoft.local>
2018-01-10 17:41:11 +0200

    header action

--- src/modules/cryptoprint/sections/index/index.js
+++ src/modules/cryptoprint/sections/index/index.js
@@ -19,7 +19,6 @@ import './index.css'
 class SectionIndex extends Component {
   constructor(props) {
     super(props)
-  console.log(props);
     this.state = {
       progress: 0,
       seed: null
